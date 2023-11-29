86fc80b43542cad9186e5826e5ad2bacfb30816c
nessie134 <ines-caps@hotmail.com>
2023-11-29 17:56:10 +0100

    player shooting + enemy spawn

--- main.cpp
+++ main.cpp
@@ -8,14 +8,34 @@ int main()
 {
 	// Initialisation
 
+	srand(time(NULL));//on initialise le random
+
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Geometry Wars");
 	window.setVerticalSyncEnabled(true);
 
 	// Début de la boucle de jeu
-	sf::CircleShape triangle (80, 3);
-	triangle.setFillColor(sf::Color::Red);
-	triangle.setPosition(640, 360);
-	//triangle.setSize(sf::Vector2f(128, 128));
+	sf::CircleShape player (30, 3);//rayon et nombre de points (3 points pour un triangle)
+	player.setRadius(30.f);
+	player.setFillColor(sf::Color::Green);
+	player.setPosition(640, 360);//au centre de l'ecran
+	sf::Vector2f playerPos;//Notre position de tir
+
+	int shootTimer = 0;
+	
+	sf::RectangleShape enemy;
+	enemy.setSize(sf::Vector2f(50.0f, 50.0f));
+	enemy.setFillColor(sf::Color::Magenta);
+	std::vector<sf::RectangleShape> enemies;
+	enemies.push_back(sf::RectangleShape(enemy));//on ajoute un ennemi à notre tableau de taille dynamique d'ennemis
+	int enemySpawnTimer = 0;
+
+	sf::CircleShape bullet;
+	bullet.setRadius(5.0f);
+	bullet.setFillColor(sf::Color::Red);
+	std::vector<sf::CircleShape> bullets;
+	bullets.push_back(sf::CircleShape(bullet));//on ajoute une balle à notre tableau de taille dynamique de projectiles
+
+
 
 	sf::Clock frameClock;
 
@@ -39,11 +59,10 @@ int main()
 		}
 
 		float deltaTime = frameClock.restart().asSeconds();
-		std::cout << 1.f / deltaTime << " FPS" << std::endl;
+		//std::cout << 1.f / deltaTime << " FPS" << std::endl;
 
 		// Logique
-		sf::Vector2f pos = triangle.getPosition();
-		
+		sf::Vector2f pos = player.getPosition();//vector2f = vecteur position avec 2 float
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
 			pos.x = pos.x - deltaTime * cubeSpeed;
 
@@ -56,7 +75,57 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 			pos.y = pos.y + deltaTime * cubeSpeed;
 
-		triangle.setPosition(pos);
+#pragma region BULLETS
+
+		player.setPosition(pos);
+
+		playerPos = sf::Vector2f(player.getPosition().x + (player.getRadius() * deltaTime), player.getPosition().y + (player.getRadius() * deltaTime));
+
+		if (shootTimer < 5) {
+
+			shootTimer++;
+		}
+
+		else if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && shootTimer >= 5)
+		{
+			shootTimer = 0;
+			bullet.setPosition(playerPos);
+			bullets.push_back(sf::CircleShape(bullet));
+		}
+		std::cout << shootTimer << "secondes" << std::endl;
+
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			bullets[i].move(0.f, -10.f);//offset x, offset y => on déplace la balle vers le bas 
+			
+			if (bullets[i].getPosition().y <= 0)//si la balle est hors de l'ecran
+			{
+				bullets.erase(bullets.begin() + i);//on supprime la balle si elle sort de l'écran. On place l'iterateur au debut
+			}
+		}
+#pragma endregion
+
+
+#pragma region BULLETS
+
+		if (enemySpawnTimer < 20) {
+
+			enemySpawnTimer++;
+		}
+		else {
+			enemy.setPosition(rand() % int (window.getSize().x - enemy.getSize().x), 0.f);// spawn aléatoire entre le bord gauche et le bord droit de l'écran
+			enemies.push_back(sf::RectangleShape(enemy));
+
+			enemySpawnTimer = 0;
+		}
+
+		for (size_t i = 0; i < enemies.size(); i++)
+		{
+			enemies[i].move(0.f, 5.f);//offset x, offset y => on déplace la balle vers le haut 
+
+		}
+#pragma endregion
+			
 
 		// Affichage
 		
@@ -64,7 +133,14 @@ int main()
 		window.clear();
 
 		// Tout le rendu va se dérouler ici
-		window.draw(triangle);
+		window.draw(player);
+
+		for (size_t i = 0; i < enemies.size(); i++) {
+			window.draw(enemies[i]);// on dessine chaque ennemi
+		}
+
+		for (size_t i = 0; i < bullets.size(); i++)
+			window.draw(bullets[i]);// on dessine chaque balle
 
 		// On présente la fenêtre sur l'écran
 		window.display();
